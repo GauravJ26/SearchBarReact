8232be71cb41c255d07b23b69e6bcb3d2aeec492
Gaurav Jaiswal <gaurav.rcet@gmail.com>
2024-06-02 13:52:23 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -8,21 +8,18 @@ function App() {
   const [query,setQuery] = useState("");
   const [data, setData] = useState([])
 
-
-
 useEffect(() => {
   const fetchUsers = async () =>{
-    const res = await axios.get(`http://localhost:5001?q=${query}`)
+  const res = await axios.get(`http://localhost:5001?q=${query}`)
   setData(res.data);
   };
 if (query.length === 0 || query.length > 2) fetchUsers()
 }, [query]);
 
-
-  return (
-    <div className="App">
+ return (
+<div className="App">
 <div className="App-header">
-<h3 className='heading1'>type something to search from the table diplayed below</h3>
+<h3 className='heading1'>Type something to search from the table displayed below</h3>
 
 <input 
 type='text' 
@@ -30,12 +27,9 @@ placeholder='Search..'
 className="search"
 onChange={(e)=>setQuery(e.target.value)}
 />
-
-
-
 <Table data={data}/>
-
-</div>    </div>
+</div>    
+</div>
   );
 }
 
